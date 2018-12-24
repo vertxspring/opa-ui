2c265b2aafcde8facdb2c98b085a5daebc6784ea
SM <vandaal05@gmail.com>
2018-12-24 08:25:44 +0530

    updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from 'src/app/second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponent, data: { title: 'First Component' } },
+  { path: 'first', component: FirstComponent, data: { title: 'First Component' } },
+  { path: 'second', component: SecondComponent, data: { title: 'Second Component' } }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialsModule} from './materials/materials.module';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
